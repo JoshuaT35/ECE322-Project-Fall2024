1a64827d6c7663a866046fdde404119070f9ea41
Joshua Tablan <joshuatablan03@gmail.com>
2024-12-04 22:52:40 -0700

    changes test files to use unittest

--- tests-Joshua/testLPSubstr.py
+++ tests-Joshua/testLPSubstr.py
@@ -1,153 +1,103 @@
 '''
-Testcases for LPSubstr() in pyrival/strings/LPSubstr.py
-
-The function in question to be tested:
-------
-def LPSubstr(s):
-    n = len(s)
-    p = [[0] * (n + 1), [0] * n]
-
-    for z, p_z in enumerate(p):
-        left, right = 0, 0
-        for i in range(n):
-            t = right - i + 1 - z
-            if i < right:
-                p_z[i] = min(t, p_z[left + t])
-            L, R = i - p_z[i], i + p_z[i] - 1 + z
-            while (L >= 1) and (R + 1 < n) and (s[L - 1] == s[R + 1]):
-                p_z[i] += 1
-                L -= 1
-                R += 1
-            if R > right:
-                left, right = L, R
-
-    i1, x1 = max(enumerate(p[0]), key=lambda x: x[1])
-    i2, x2 = max(enumerate(p[1]), key=lambda x: x[1])
-
-    return s[i1 - x1:i1 + x1], s[i2 - x2:i2 + x2 + 1]
-------
-    
-PyRival is credited as the source of the function.
-
-NOTE: since no unreliable dependencies can be seen in the function,
-we do not require unittest.
+Testcases for LPSubstr()
 '''
 
 # import libraries
-from pyrival.strings import LPSubstr
-import pandas as pd
+import unittest
+from LPSubstr import LPSubstr
 
-def main():
-    # strings
-    test_id_str = "Test ID"
-    description_str = "Description"
-    input_str = "Input"
-    expected_str = "Expected Result"
-    actual_str = "Actual Result"
+class TestLpSubstr(unittest.TestCase):
 
-    # create table to hold results
-    results = pd.DataFrame(
-        columns=[
-            test_id_str,
-            description_str,
-            input_str,
-            expected_str,
-            actual_str
-        ]
-    )
+    def testSingleCharacter(self):
+        input_str = "a"
+        expected_value = ('', 'a')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-    # create description
-    test_case_description = [
-        "Single character",
-        "No palindrome longer than 1 character",
-        "Entire string is a palindrome",
-        "Palindrome with even length",
-        "Palindrome with odd length",
-        "Mixed case palindrome",
-        "Long string with palindromes",
-        "Multiple palindromes: even & odd",
-        "Multiple palindromes: even & even, same length",
-        "Multiple palindromes: even & even, different length",
-        "Multiple palindromes: odd & odd, same length",
-        "Multiple palindromes: odd & odd, different length",
-        "Multiple palindromes part 1: same length, first palindrome used",
-        "Multiple palindromes part 2: same length, first palindrome used",
-        "Empty string",
-        "String with no palindromes"
-    ]
+    def testNoPalindromeLongerThanOne(self):
+        input_str = "abc"
+        expected_value = ('', 'a')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-    # create input
-    test_case_input = [
-        "a",
-        "abc",
-        "racecar",
-        "abba",
-        "madam",
-        "AbcCba",
-        "abcdcbac",
-        "abbamadam",
-        "abbaqwwq",
-        "abbaqwffwq",
-        "madamxyzyx",
-        "madamwxyzyxw",
-        "abazyz",
-        "zyzaba",
-        "",
-        "xyz"
-    ]
+    def testEntireStringIsPalindrome(self):
+        input_str = "racecar"
+        expected_value = ('', 'racecar')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-    # create expected result
-    test_case_expected = [
-        ('', 'a'),
-        ('', 'a'),
-        ('', 'racecar'),
-        ('abba', 'a'),
-        ('', 'madam'),
-        ('', 'A'),
-        ('', 'abcdcba'),
-        ('abba', 'madam'),
-        ('abba', 'a'),
-        ('qwffwq', 'a'),
-        ('', 'madam'),
-        ('', 'wxyzyxw'),
-        ('', 'aba'),
-        ('', 'zyz'),
-        ('', ''),
-        ('x', 'y')
-    ]
+    def testPalindromeWithEvenLength(self):
+        input_str = "abba"
+        expected_value = ('abba', 'a')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-    # assert that there is message, input, and expectation for each result
-    assert len(test_case_description) == len(test_case_input)
-    assert len(test_case_description) == len(test_case_expected)
-    assert len(test_case_input) == len(test_case_expected)
+    def testPalindromeWithOddLength(self):
+        input_str = "madam"
+        expected_value = ('', 'madam')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
+    def testMixedCasePalindrome(self):
+        input_str = "AbcCba"
+        expected_value = ('', 'AbcCba')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-    # iterate through test cases
-    for rowIdx in range(0, len(test_case_description)):
+    def testLongStringWithPalindromes(self):
+        input_str = "abcdcbac"
+        expected_value = ('', 'abcdcba')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-        # add idx
-        idx = rowIdx + 1
-        results.loc[rowIdx, test_id_str] = idx
+    def testMultiplePalindromesEvenOdd(self):
+        input_str = "abbamadam"
+        expected_value = ('abba', 'madam')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-        # add description
-        results.loc[rowIdx, description_str] = test_case_description[rowIdx]
+    def testMultiplePalindromesEvenEvenSameLength(self):
+        input_str = "abbaqwwq"
+        expected_value = ('abba', 'a')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-        # add input
-        results.loc[rowIdx, input_str] = test_case_input[rowIdx]
+    def testMultiplePalindromesEvenEvenDifferentLength(self):
+        input_str = "abbaqwffwq"
+        expected_value = ('qwffwq', 'a')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-        # add expected
-        results.loc[rowIdx, expected_str] = test_case_expected[rowIdx]
+    def testMultiplePalindromesOddOddSameLength(self):
+        input_str = "madamxyzyx"
+        expected_value = ('', 'madam')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-        # add actual
-        try:
-            results.loc[rowIdx, actual_str] = LPSubstr.LPSubstr(test_case_input[rowIdx])
-        except:
-            results.loc[rowIdx, actual_str] = "ERROR THROWN BY PROGRAM"
-    
-    # write result to excel file
-    results.to_excel("lpsubstr.xlsx")
+    def testMultiplePalindromesOddOddDifferentLength(self):
+        input_str = "madamwxyzyxw"
+        expected_value = ('', 'wxyzyxw')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
+    def testMultiplePalindromesPart1SameLengthFirstPalindromeUsed(self):
+        input_str = "abazyz"
+        expected_value = ('', 'aba')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
 
-if __name__ == "__main__":
-    main()
+    def testMultiplePalindromesPart2SameLengthFirstPalindromeUsed(self):
+        input_str = "zyzaba"
+        expected_value = ('', 'zyz')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
+
+    def testEmptyString(self):
+        input_str = ""
+        expected_value = ('', '')
+        actual_value = LPSubstr(input_str)
+        self.assertEqual(expected_value, actual_value)
+
+if __name__ == '__main__':
+    unittest.main()
     
